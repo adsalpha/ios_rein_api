d277e73dd4330f8f087f9ee504a9cdb7fa954e3f
Alexander Dushkin <ads2alpha@gmail.com>
2017-03-16 18:02:43 +0200

    Change NGINX template location

--- nginx/init/init.sh
+++ nginx/init/init.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 export DOLLAR='$'
-envsubst < /etc/nginx/sites-available/${URL}.conf.template > /etc/nginx/sites-enabled/${URL}.conf
-envsubst < /etc/nginx/sites-enabled/${URL}.conf.template > /etc/nginx/sites-enabled/${URL}.conf
+envsubst < /init/${URL}.conf.template > /etc/nginx/sites-enabled/${URL}.conf
+envsubst < /init/${URL}.conf.template > /etc/nginx/sites-enabled/${URL}.conf
 nginx -g "daemon off;"
